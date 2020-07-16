46de7f34b10458957f452437887e80f179fafce4
NubsPixel <nubspixel@gmail.com>
2020-07-17 00:09:27 +0800

    Added an extra option that generates a chatlog for GM and Player instance. The auto save chat log saves faster than the normal chatlog. Also clean up the html codes so we maybe able to process it later.

--- src/main/java/net/rptools/maptool/client/ChatAutoSave.java
+++ src/main/java/net/rptools/maptool/client/ChatAutoSave.java
@@ -35,11 +35,19 @@ public class ChatAutoSave {
   private long delay;
   private static String chatlog = null;
 
+  private final Timer fast_countdown;
+  private TimerTask fast_task;
+  private long fast_delay;
+  private static String fast_chatlog = null;
+
   private ChatAutoSave() {
     log.debug("Creating chat log autosave timer"); // $NON-NLS-1$
     // Only way to set the delay is to call changeTimeout()
     delay = 0;
     countdown = new Timer();
+
+    fast_delay = 0;
+    fast_countdown = new Timer();
   }
 
   private static TimerTask createTimer(final long timeout) {
@@ -96,6 +104,9 @@ public class ChatAutoSave {
   public static void changeTimeout(int timeout) {
     getInstance().delay = timeout * 1000 * 60;
     getInstance().start();
+
+    getInstance().fast_delay = timeout * 1000;
+    getInstance().fast_start();
   }
 
   private void stop() {
@@ -110,6 +121,89 @@ public class ChatAutoSave {
       stop();
       task = createTimer(delay);
       countdown.schedule(task, 5000, delay); // Wait 5s, then save the log every 'delay' ms
+    }
+  }
+
+  private static TimerTask fast_createTimer(final long timeout) {
+    TimerTask t =
+        new TimerTask() {
+          @Override
+          public void run() {
+            // if (fast_chatlog == null) {
+            //  String filename = "c"+AppPreferences.getChatFilenameFormat();
+            //  // FJE Ugly kludge to replace older default entry with newer default
+            //  // TODO This is going into 1.3.b77 so remove it in 3-4 builds
+            //  if ("cchatlog.html".equals(filename)) { // $NON-NLS-1$
+            //    AppPreferences.clearChatFilenameFormat();
+            //    filename = AppPreferences.getChatFilenameFormat();
+            //  }
+            //  fast_chatlog = String.format(filename, new Date()).replace(':', '-');
+            // }
+
+            if (MapTool.getPlayer().isGM() && MapTool.isHostingServer())
+              fast_chatlog = "chatlog_gm.txt";
+            else {
+              fast_chatlog = "chatlog_player.txt";
+            }
+
+            File chatFile =
+                new File(AppUtil.getAppHome("autosave").toString(), fast_chatlog); // $NON-NLS-1$
+
+            CommandPanel chat = MapTool.getFrame().getCommandPanel();
+            String old = MapTool.getFrame().getStatusMessage();
+            try {
+              MapTool.getFrame()
+                  .setStatusMessage(
+                      I18N.getString("ChatAutoSave.status.chatAutosave")); // $NON-NLS-1$
+              try (FileWriter writer = new FileWriter(chatFile)) {
+                String chatmsg = chat.getMessageHistory();
+                String find_regex = "(?:[ ]*)</?html>(?:[ \n]*)|";
+                find_regex += "(?:[ ]*)</?head>(?:[ \n]*)|";
+                find_regex += "(?:[ ]*)</?style(?:[ a-z0-9=\"/]*)>(?:[ \n]*)|";
+                find_regex += "(?:[ ]*)<!--(?:[.* a-zA-Z0-9-{:;}#\n]*)-->(?:[ \n]*)|";
+                find_regex += "(?:[ ]*)</?body(?:[ a-z0-9=\"/]*)>(?:[ \n]*)|";
+                find_regex += "(?:[ ]*)</?div>(?:[ \n]*)|";
+                find_regex += "(?:[ ]*)</?table(?:[ a-z0-9=\"]*)>(?:[ \n]*)|";
+                find_regex += "(?:[ ]*)</?tr>(?:[ \n]*)|";
+                find_regex += "(?:[ ]*)</?td(?:[ a-zA-Z0-9-\":;\"=]*)>(?:[ \n]*)|";
+                find_regex += "(?:[ ]*)<img src=\"(?:[a-z0-9-:/]*)\">(?:[ \n]*)|";
+                // find_regex += "(?:[ \n]{2,99})";
+                chatmsg = chatmsg.replaceAll(find_regex, "");
+                chatmsg = chatmsg.replaceAll("(?:[ \n]{2,99})", " ");
+
+                // chatmsg = "\nisGM: "+MapTool.getPlayer().isGM()+" | isHostingServer"+
+                // MapTool.isHostingServer() +" | isPersonalServer"+ MapTool.isPersonalServer()
+                // +"\n";
+                writer.write(chatmsg);
+              }
+            } catch (IOException e) {
+              // If this happens should we track it and turn off the autosave? Perhaps
+              // after a certain number of consecutive failures? Or maybe just lengthen
+              // the amount of time between attempts in that case? At a minimum we
+              // should probably give the user a chance to turn it off as part of this
+              // message box that pops up...
+              MapTool.showWarning("msg.warn.failedAutoSavingMessageHistory", e); // $NON-NLS-1$
+            } finally {
+              MapTool.getFrame().setStatusMessage(old);
+            }
+          }
+        };
+    return t;
+  }
+
+  private void fast_stop() {
+    if (fast_task != null) {
+      fast_task.cancel();
+      fast_task = null;
+    }
+  }
+
+  private void fast_start() {
+    if (fast_delay > 0) {
+      fast_stop();
+      fast_task = fast_createTimer(fast_delay);
+      fast_countdown.schedule(
+          fast_task, 1000, fast_delay); // Wait 5s, then save the log every 'delay' ms
     }
   }
 }
